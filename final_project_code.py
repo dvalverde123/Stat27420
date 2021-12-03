65ac7c82bca87c176f50d37347353252e4db86c9
Dani Valverde <danivalverde2000@gmail.com>
2021-12-03 13:03:44 -0600

    added skeleton code except for dif in dif

--- final_project_code.py
+++ final_project_code.py
@@ -48,24 +48,20 @@ np.random.seed(RANDOM_SEED)
 
 # choose model for the conditional expected outcome
 
-# random forest model that returns sklearn model for later use in k-folding
-# def create_random_forest_Q():
-#    return RandomForestRegressor(random_state=RANDOM_SEED, n_estimators=500, max_depth=None)
-
 # gradient boosting model 
-#def create_xgb_Q():
-    #return XGBClassifier
+def create_xgb_Q():
+    return XGBClassifier
 
 # linear regression model 
 # https://scikit-learn.org/stable/modules/linear_model.html
-#def create_linear_regression_Q():
-    #return "hi"
+def create_linear_regression_Q():
+    return "hi"
 
 
 # k-nearest_neighbors
 # https://scikit-learn.org/stable/modules/neighbors.html
-#def create_k_nearest_neighbors_Q():
-    #return 'HI'
+def create_k_nearest_neighbors_Q():
+    return 'HI'
 
 # random_forest
 
@@ -124,6 +120,63 @@ baseline_cross_entropy = log_loss(A_test, A_Train.mean()*np.ones_like(A_test))
 
 # could do cross fitting here? Idt it will work tho 
 
+def treatment_k_fold_fit_predict(make_model, X:pd.DataFrame, A:np.array, n_splits:int):
+    predictions = np.full_like(A, np.nan, dtype=float)
+    kf = StratifiedKFold(n_splits=n_splits, shuffle=True, random_state=RANDOM_SEED)
+
+    for train_indx, test_index in kf.split(X,A):
+        X_train = X.loc[train_index]
+        A_train = A.loc[train_index]
+        g = make_model()
+        g.fit(X_train, A_train)
+
+        predictions[test_index] = g.predict_proba(X.loc[test_idex])[:,1]
+
+    assert np.isnan(predictions).sum() == 0
+    return predictions
+
+def outcome_k_fold_fit_predict:
+    predictions0 = np.full_like(A, np.nan, dtype=float)
+    predictions1 = np.full_like(y, np.nan, dtype=float)
+    if output_type == 'binary':
+        kf = StratifiedKFold(n_splits=n_splits, shuffle=True, random_state=RANDOM_SEED)
+    elif output_type == 'continuous':
+        kf = KFold(n_splits=n_splits, shuffle=True, random_state=RANDOM_SEED)
+
+    X_w_treatment = X.copy()
+    X_w_treatment["A"] = A
+
+    X0 = X_w_treatment.copy()
+    X0["A"] = 0
+    X1 = X_w_treatment.copy()
+    X1["A"] = 1
+
+    for train_index, test_index in kf.split(X_w_treatment, y):
+        X_train = X_w_treatment.loc[train_index]
+        y_train = y.loc[train_index]
+        q = make_model()
+        q.fit(X_train, y_train)
+
+        if output_type == 'binary':
+            predictions0[test_index] = q.predict_proba(X0.loc[test_index])[:, 1]
+            predictions1[test_index] = q.predict_proba(X1.loc[test_index])[:, 1]
+        elif output_type == 'continuous':
+            predictions0[test_index] = q.predict(X0.loc[test_index])
+            predictions1[test_index] = q.predict(X1.loc[test_index])
+
+    assert np.isnan(predictions0).sum() == 0
+    assert np.isnan(predictions1).sum() == 0
+    return predictions0, predictions1
+
+g = treatment_k_fold_fit_predict(make_g_model, X=confounders, A=treatment, n_splits=10)
+
+Q0,Q1=outcome_k_fold_fit_predict(make_Q_model, X=confounders, y=outcome, A=treatment, n_splits=10, output_type='continuous')
+
+data_nuisance_estimates = pd.DataFrame({'g': g, 'Q0': Q0, 'Q1': Q1, 'A': treatment, 'Y': outcome})
+data_nuisance_estimates.head()
+
+
+
 # Double ML estimator for ATT
 def att_aiptw(Q0, Q1, g, A, Y, prob_t=None):
     if prob_t is None:
@@ -144,24 +197,75 @@ def ate_aiptw(Q0, Q1, g, A, Y, prob_t=None):
     
     return tau_hat, std_hat
 
+in_treated = data_nuisance_estimates['A']==1
+treated_estimates = data_nuisance_estimates[in_treated]
+tau_hat, std_hat = ate_aiptw(**treated_estimates)
+
+print(f"The estimate is {tau_hat} pm {1.96*std_hat}")
+
+# address overlap issues here 
+
 # Double ML Library Estimation Procedure 
+
+
 
 # Differences in Differences Estimation 
 
+tau_hat, std_hat = att_aiptw(**data_nuisance_estimates)
+
+# point estimate without covariate correction
+
+outcome[treatment==1].mean()-outcome[treatment==0.mean()]
+
+# Sensitivity Analysis
+
+# create covariate groups 
+
+covariate_groups = {
+    'economic':
+    'population':
+    'age':
+    'health':
+}
+
+# refit models for each covariate group 
+
+nuisance_estimates = {}
+
+for group, covariates in covariate_groups.items():
+    remaining_confounders = confounders.drop(columns=covariates)
+
+    g = treatment_k_fold_fit_predict(make_g_model, X=remaining_confounders)
+    Q0, Q1 = outcome_k_fold_fit_predict(make_Q_model, X=remaining_confounders)
+    data_nuisance_estimates = pd.DataFrame(({'g': g, 'Q0': Q0, 'Q1': Q1, 'A': treatment, 'Y': outcome}))
+    nuisance_estimates[group] = data_nuisance_estimates
+
+data_nuisance_path = 'data_nuisance_estimates.csv'
+covariate_direct_path = 'covariates/'
+
+def convert_to_austen_format(nuisance_estimate_df: pd.DataFrame):
+    austen_df = pd.DataFrame()
+    austen_df['y']=nuisance_estimate_df['Y']
+    austen_df['t']=nuisance_estimate_df['A']
+    austen_df['g']=nuisance_estimate_df['g']
+    A = nuisance_estimate_df['A']
+    austen_df['Q']=A*nuisance_estimate_df['Q1'] + (1-A)*nuisance_estimate_df['Q0']
+
+austen_data_nuisance = convert_to_austen_format(data_nuisance_estimate)
+austen_data_and_nuisance.to_csv(data_and_nuisance_apth, index=False)
+
+pathlib.Path(covariate_dir_path).mkdir(exist_ok=True)
+for group, nuisance_estimate in nuisance_estimates.items():
+    austen_nuisance_estimate = convert_to_austen_format(nuisance_estimate)
+    austen_nuisance_estimate.to_csv(os.path.join(covariate_dir_path+'.csv'), index=False)
+
 # Austen Plots 
 target_bias = 15.0 
-covariates = {}
-estimates_for_nuisance = {}
-for group, covariates in covariates.items():
-    narrowed_confounders = confounders.drop(columns = covariates)
-    
-g = treatment_k_fold_fit_and_predict(create_g_model, X = narrowed_confounders, A = treatment, n_splits = 5)
-Q0, Q1 = outcome_k_fold_fit_and_predict()
 
-data_nuisance_estimates = pd.DataFrame(({'g': g, 'Q0': Q0, 'Q1': Q1, 'A': treatment, 'Y': outcome}))
-nuisance_estimates[group] = data_nuisance_estimates
+
 austen_plot = AustenPlot(data_nuisance, covariate_path)
 p, plot_cooredinates, variable_coordinates = austen_plot.fit(bias = target_bias)
+p
 
 """
 
